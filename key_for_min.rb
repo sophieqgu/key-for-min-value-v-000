dc237686c4bf084c362ff4b85ef39cd71d2cd689
Sophie Gu <sophieqgu@gmail.com>
2018-10-11 16:41:24 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   min_value = 0
   name_hash.each do |name, value|
-    min_value = value if value < min_value || min_value = 0
+    min_value = value unless value > min_value 
   end 
   name_hash[min_value]
 end
